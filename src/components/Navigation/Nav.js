4c6abaf2c7f40a394931c0b9e20a5d0eda7ba34b
Joy Rhee <joyfullyx@gmail.com>
2021-11-01 14:37:33 -0700

    center nav items

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
+import Box from "@mui/material/Box";
 
 export default function BasicMenu() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -15,81 +16,91 @@ export default function BasicMenu() {
 
   return (
     <div>
-      <Button
-        id="basic-button"
-        aria-controls="basic-menu"
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        // onClick={handleClick}
-      >
-        About
-      </Button>
-      <Button
-        id="basic-button"
-        aria-controls="basic-menu"
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        onMouseOver={handleClick}
-      >
-        Courses
-      </Button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          "aria-labelledby": "basic-button",
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-around",
+          p: 1,
         }}
       >
-        <MenuItem onClick={handleClose}>Interbay Golf Center</MenuItem>
-        <MenuItem onClick={handleClose}>Jackson Park Golf Course</MenuItem>
-        <MenuItem onClick={handleClose}>Jefferson Park Golf Course</MenuItem>
-        <MenuItem onClick={handleClose}>West Seattle Golf Course</MenuItem>
-        <MenuItem onClick={handleClose}>Bellevue Golf Course</MenuItem>
-        <MenuItem onClick={handleClose}>Crossroads Par 3 Golf Course</MenuItem>
-        <MenuItem onClick={handleClose}>Legion Memorial Golf Course</MenuItem>
-        <MenuItem onClick={handleClose}>Walter E. Hall Golf Course</MenuItem>
-        <MenuItem onClick={handleClose}>Lynnwood Golf Course</MenuItem>
-        <MenuItem onClick={handleClose}>Cedarcrest Golf Course</MenuItem>
-        <MenuItem onClick={handleClose}>Lake Padden Golf Course</MenuItem>
-      </Menu>
-      <Button
-        id="basic-button"
-        aria-controls="basic-menu"
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        // onClick={handleClick}
-      >
-        PGC Instruction Academy
-      </Button>
-      <Button
-        id="basic-button"
-        aria-controls="basic-menu"
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        // onClick={handleClick}
-      >
-        Footgolf
-      </Button>
-      <Button
-        id="basic-button"
-        aria-controls="basic-menu"
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        // onClick={handleClick}
-      >
-        Daily Deals
-      </Button>
-      <Button
-        id="basic-button"
-        aria-controls="basic-menu"
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        // onClick={handleClick}
-      >
-        Fall Aerification
-      </Button>
+        <Button
+          id="basic-button"
+          aria-controls="basic-menu"
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          // onClick={handleClick}
+        >
+          About
+        </Button>
+        <Button
+          id="basic-button"
+          aria-controls="basic-menu"
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onMouseOver={handleClick}
+        >
+          Courses
+        </Button>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <MenuItem onClick={handleClose}>Interbay Golf Center</MenuItem>
+          <MenuItem onClick={handleClose}>Jackson Park Golf Course</MenuItem>
+          <MenuItem onClick={handleClose}>Jefferson Park Golf Course</MenuItem>
+          <MenuItem onClick={handleClose}>West Seattle Golf Course</MenuItem>
+          <MenuItem onClick={handleClose}>Bellevue Golf Course</MenuItem>
+          <MenuItem onClick={handleClose}>
+            Crossroads Par 3 Golf Course
+          </MenuItem>
+          <MenuItem onClick={handleClose}>Legion Memorial Golf Course</MenuItem>
+          <MenuItem onClick={handleClose}>Walter E. Hall Golf Course</MenuItem>
+          <MenuItem onClick={handleClose}>Lynnwood Golf Course</MenuItem>
+          <MenuItem onClick={handleClose}>Cedarcrest Golf Course</MenuItem>
+          <MenuItem onClick={handleClose}>Lake Padden Golf Course</MenuItem>
+        </Menu>
+        <Button
+          id="basic-button"
+          aria-controls="basic-menu"
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          // onClick={handleClick}
+        >
+          PGC Instruction Academy
+        </Button>
+        <Button
+          id="basic-button"
+          aria-controls="basic-menu"
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          // onClick={handleClick}
+        >
+          Footgolf
+        </Button>
+        <Button
+          id="basic-button"
+          aria-controls="basic-menu"
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          // onClick={handleClick}
+        >
+          Daily Deals
+        </Button>
+        <Button
+          id="basic-button"
+          aria-controls="basic-menu"
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          // onClick={handleClick}
+        >
+          Fall Aerification
+        </Button>
+      </Box>
     </div>
   );
 }
