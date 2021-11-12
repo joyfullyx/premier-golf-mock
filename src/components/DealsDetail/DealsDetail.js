ce08b70b91671c707a119763ed91f857dfd918d3
Joy Rhee <joyfullyx@gmail.com>
2021-11-12 15:42:15 -0800

    clean up commented out things

--- src/components/DealsDetail/DealsDetail.js
+++ src/components/DealsDetail/DealsDetail.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import {
   Grid,
   ImageList,
-  Box,
-  Paper,
   Card,
   CardActions,
   CardContent,
@@ -40,12 +38,9 @@ export default function DealsDetail() {
   const styles = {
     mediaStyles: {
       height: 160,
-      // paddingTop: "56.25%",
       marginTop: "30",
     },
     cardStyles: {
-      // justfifyContent: "space-around",
-      // alignItems: "center",
       maxWidth: 300,
       margin: "auto",
     },
@@ -76,18 +71,14 @@ export default function DealsDetail() {
             <Card
               sx={{
                 maxWidth: 400,
-                // justifyContent: "space-between",
                 alignSelf: "center",
               }}
               style={styles.cardStyles}
             >
               <CardMedia
                 component="img"
-                // height="309"
                 image={`${deals.image}`}
                 style={styles.mediaStyles}
-                // imageUrl={`${deals.image}`}
-                // src={`${deals.image}`}
                 alt="course logo and name"
               />
               <CardContent style={styles.contentStyles}>
