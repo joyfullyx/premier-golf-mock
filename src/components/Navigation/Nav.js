52f3d41ef58fba39bdfa186d6c67b2ad6c0aefd8
Joy Rhee <joyfullyx@gmail.com>
2021-11-18 12:07:22 -0800

    refactor nav menu

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
-import { Button, Menu, Box, List, ListItem, ListItemText } from "@mui/material";
+import {
+  Button,
+  Menu,
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  MenuItem,
+} from "@mui/material";
 import PremierGolfLogo from "../../assets/PremierGolfLogo.jpg";
+import { navMenuItems } from "./navMenuItems";
 
 export default function BasicMenu() {
   const [anchorEl, setAnchorEl] = useState(null);
-  const [about, setAbout] = useState(null);
   const open = Boolean(anchorEl);
-  const openAbout = Boolean(about);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-    setAbout(event.currentTarget);
+
+  // const handleClick = (event) => {
+  //   setAnchorEl(event.currentTarget);
+  // };
+  const handleClick = (index, event) => {
+    setAnchorEl({ [index]: event.currentTarget });
   };
   const handleClose = () => {
     setAnchorEl(null);
@@ -29,7 +39,7 @@ export default function BasicMenu() {
       <Box
         sx={{
           display: "flex",
-          justifyContent: "space-between",
+          // justifyContent: "space-between",
           p: 1,
         }}
       >
@@ -45,78 +55,184 @@ export default function BasicMenu() {
             p: 1,
           }}
         >
-          <Button
-            style={styles.buttonStyle}
-            id="basic-button"
-            aria-controls="basic-menu"
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            onMouseOver={handleClick}
-          >
-            COURSES
-          </Button>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-button",
-            }}
-          >
-            <List>
-              {[
-                "Interbay Golf Center",
-                "Jackson Park Golf Course",
-                "Jefferson Park Golf Course",
-                "West Seattle Golf Course",
-                "Bellevue Golf Course",
-                "Crossroads Par 3 Golf Course",
-                "Legion Memorial Golf Course",
-                "Walter E. Hall Golf Course",
-                "Lynnwood Golf Course",
-                "Cedarcrest Golf Course",
-                "Lake Padden Golf Course",
-              ].map((text) => (
-                <ListItem button key={text}>
-                  <ListItemText primary={text} />
-                </ListItem>
-              ))}
-            </List>
-          </Menu>
-          <Button
-            style={styles.buttonStyle}
-            id="basic-button"
-            aria-controls="basic-menu"
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            // onClick={handleClick}
-            onMouseOver={handleClick}
-          >
-            ABOUT
-          </Button>
-          <Button
-            style={styles.buttonStyle}
-            id="basic-button"
-            aria-controls="basic-menu"
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            // onClick={handleClick}
-          >
-            DEALS
-          </Button>
-          <Button
-            style={styles.buttonStyle}
-            id="basic-button"
-            aria-controls="basic-menu"
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            // onClick={handleClick}
-          >
-            JOIN PREMIER GC
-          </Button>
+          {/* map over nav menu titles by index from navMenuItems */}
+          {Object.keys(navMenuItems).map((item, index) => (
+            <div key={index}>
+              <Button
+                style={styles.buttonStyle}
+                id="basic-button"
+                aria-controls="basic-menu"
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onMouseOver={(e) => handleClick(index, e)}
+              >
+                {item}
+              </Button>
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl && anchorEl[index]}
+                open={open && anchorEl[index]}
+                onClose={handleClose}
+                MenuListProps={{
+                  "aria-labelledby": "basic-button",
+                }}
+              >
+                {/* map over drop down menu items from navMenuItems */}
+                {navMenuItems[item].map((navMenuItems, navMenuIndex) => (
+                  <MenuItem
+                    key={navMenuIndex}
+                    selected={navMenuItems === item}
+                    onClose={handleClose}
+                  >
+                    {navMenuItems.title}
+                  </MenuItem>
+                ))}
+              </Menu>
+            </div>
+          ))}
         </Box>
+        {/* <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            p: 1,
+          }}
+        > */}
+        <Button style={styles.buttonStyle}>DEALS</Button>
+        <Button style={styles.buttonStyle}>JOIN PREMIER GC</Button>
+        {/* </Box> */}
       </Box>
     </div>
   );
+
+  // return (
+  //   <div>
+  //     <Box
+  //       sx={{
+  //         display: "flex",
+  //         justifyContent: "space-between",
+  //         p: 1,
+  //       }}
+  //     >
+  //       <img
+  //         src={PremierGolfLogo}
+  //         alt="premier golf logo"
+  //         style={styles.logoStyle}
+  //       />
+  //       <Box
+  //         sx={{
+  //           display: "flex",
+  //           justifyContent: "flex-end",
+  //           p: 1,
+  //         }}
+  //       >
+  //         <Button
+  //           style={styles.buttonStyle}
+  //           id="basic-button"
+  //           aria-controls="basic-menu"
+  //           aria-haspopup="true"
+  //           aria-expanded={open ? "true" : undefined}
+  //           onMouseOver={handleClick}
+  //         >
+  //           COURSES
+  //         </Button>
+  //         <Menu
+  //           id="basic-menu"
+  //           anchorEl={anchorEl}
+  //           open={open}
+  //           onClose={handleClose}
+  //           MenuListProps={{
+  //             "aria-labelledby": "basic-button",
+  //           }}
+  //         >
+  //           <List>
+  //             {[
+  //               "Interbay Golf Center",
+  //               "Jackson Park Golf Course",
+  //               "Jefferson Park Golf Course",
+  //               "West Seattle Golf Course",
+  //               "Bellevue Golf Course",
+  //               "Crossroads Par 3 Golf Course",
+  //               "Legion Memorial Golf Course",
+  //               "Walter E. Hall Golf Course",
+  //               "Lynnwood Golf Course",
+  //               "Cedarcrest Golf Course",
+  //               "Lake Padden Golf Course",
+  //             ].map((text) => (
+  //               <ListItem button key={text}>
+  //                 <ListItemText primary={text} />
+  //               </ListItem>
+  //             ))}
+  //           </List>
+  //         </Menu>
+  //         <Button
+  //           style={styles.buttonStyle}
+  //           id="basic-button"
+  //           aria-controls="basic-menu"
+  //           aria-haspopup="true"
+  //           aria-expanded={open ? "true" : undefined}
+  //           onClick={handleClick}
+  //           onMouseOver={handleClick}
+  //         >
+  //           ABOUT
+  //         </Button>
+  //         <Menu
+  //           id="basic-menu"
+  //           anchorEl={anchorEl}
+  //           // open={open}
+  //           onClose={handleClose}
+  //           MenuListProps={{
+  //             "aria-labelledby": "basic-button",
+  //           }}
+  //         >
+  //           <List>
+  //             {[
+  //               "Mission statement",
+  //               "PGC instruction academy",
+  //               "Footgolf",
+  //               "News",
+  //               "Employment",
+  //               "Premier Club",
+  //               "Join Premier GC",
+  //               "Contact Us",
+  //             ].map((text) => (
+  //               <ListItem button key={text}>
+  //                 <ListItemText primary={text} />
+  //               </ListItem>
+  //             ))}
+  //           </List>
+  //         </Menu>
+  //         <Button
+  //           style={styles.buttonStyle}
+  //           id="basic-button"
+  //           aria-controls="basic-menu"
+  //           aria-haspopup="true"
+  //           aria-expanded={open ? "true" : undefined}
+  //           // onClick={handleClick}
+  //         >
+  //           DEALS
+  //         </Button>
+  //         <Button
+  //           style={styles.buttonStyle}
+  //           id="basic-button"
+  //           aria-controls="basic-menu"
+  //           aria-haspopup="true"
+  //           aria-expanded={open ? "true" : undefined}
+  //           // onClick={handleClick}
+  //         >
+  //           JOIN PREMIER GC
+  //         </Button>
+  //         <Button
+  //           style={styles.buttonStyle}
+  //           id="basic-button"
+  //           aria-controls="basic-menu"
+  //           aria-haspopup="true"
+  //           aria-expanded={open ? "true" : undefined}
+  //         >
+  //           MY ACCOUNT
+  //         </Button>
+  //       </Box>
+  //     </Box>
+  //   </div>
+  // );
 }
---
+++ src/components/Navigation/navMenuItems.js
@@ -0,0 +1,60 @@
+export const navMenuItems = {
+  courses: [
+    {
+      title: "Interbay Golf Center",
+    },
+    {
+      title: "Jackson Park Golf Course",
+    },
+    {
+      title: "Jefferson Park Golf Course",
+    },
+    {
+      title: "West Seattle Golf Course",
+    },
+    {
+      title: "Bellevue Golf Course",
+    },
+    {
+      title: "Crossroads Par 3 Golf Course",
+    },
+    {
+      title: "Legion Memorial Golf Course",
+    },
+    {
+      title: "Walter E. Hall Golf Course",
+    },
+    {
+      title: "Lynnwood Golf Course",
+    },
+    {
+      title: "Cedarcrest Golf Course",
+    },
+    {
+      title: "Lake Padden Golf Course",
+    },
+  ],
+  about: [
+    {
+      title: "Mission Statement",
+    },
+    {
+      title: "PGC Instruction Academy",
+    },
+    {
+      title: "News",
+    },
+    {
+      title: "Employment",
+    },
+    {
+      title: "Premier Club",
+    },
+    {
+      title: "Join Premier GC",
+    },
+    {
+      title: "Contact Us",
+    },
+  ],
+};
